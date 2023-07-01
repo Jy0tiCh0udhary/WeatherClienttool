db207799580f352105c4f0e53b7d5cfd515d31a5
Jyoti Choudhary <jyoti.choudhary10@outlook.com>
2023-07-01 16:16:41 +0530

    Initial commit

---
+++ WeatherClientTool/Model/WeatherData.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherClientTool.Model
+{
+    public class WeatherData
+    {
+        [JsonProperty("latitude")]
+        public decimal Latitude { get; set; }
+
+        [JsonProperty("longitude")]
+        public decimal Longitude { get; set; }
+
+        [JsonProperty("current_weather")]
+        public CurrentWeatherData CurrentWeather { get; set; }
+    }
+
+    public class CurrentWeatherData
+    {
+        [JsonProperty("temperature")]
+        public double Temperature { get; set; }
+
+        [JsonProperty("windspeed")]
+        public double Windspeed { get; set; }
+
+    }
+
+    public class CityData
+    {
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("lat")]
+        public decimal Latitude { get; set; }
+
+        [JsonProperty("lng")]
+        public decimal Longitude { get; set; }
+    }
+
+}
---
+++ WeatherClientTool/Program.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+using WeatherClientTool.Model;
+
+namespace WeatherClientTool
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Write("Enter a city name: ");
+            string? cityName = Console.ReadLine();
+
+            string jsonFileContent = File.ReadAllText("cities.json");
+            List<CityData> cityList = JsonConvert.DeserializeObject<List<CityData>>(jsonFileContent);
+            CityData? city = cityList.FirstOrDefault(c => c.City.Equals(cityName, StringComparison.OrdinalIgnoreCase));
+
+            if (city != null)
+            {
+                decimal latitude = city.Latitude;
+                decimal longitude = city.Longitude;
+                WeatherData weatherData = await FetchWeatherData(latitude, longitude);
+                Console.WriteLine($"Temperature: {weatherData.CurrentWeather.Temperature}");
+                Console.WriteLine($"Windspeed: {weatherData.CurrentWeather.Windspeed}");
+            }
+            else
+            {
+                Console.WriteLine("City not found.");
+            }         
+
+        }
+
+        public static async Task<WeatherData> FetchWeatherData(decimal latitude, decimal longitude)
+        {
+            string apiUrl = $"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                WeatherData weatherData = JsonConvert.DeserializeObject<WeatherData>(jsonResponse);
+
+                return weatherData;
+            }
+        }
+
+    }
+
+}
+
